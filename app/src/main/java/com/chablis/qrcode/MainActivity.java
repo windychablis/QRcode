1b97185d35755e116c1a1e37d44cb0e53afffc16
chablis <windychablis@hotmail.com>
2017-02-22 14:49:45 +0800

    二维码扫描

---
+++ app/src/main/java/com/chablis/qrcode/MainActivity.java
@@ -0,0 +1,64 @@
+package com.chablis.qrcode;
+
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.bingoogolapple.qrcode.core.QRCodeView;
+import cn.bingoogolapple.qrcode.zxing.ZXingView;
+
+public class MainActivity extends AppCompatActivity implements QRCodeView.Delegate {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_CHOOSE_QRCODE_FROM_GALLERY = 666;
+
+    private QRCodeView mQRCodeView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mQRCodeView = (ZXingView) findViewById(R.id.zxingview);
+        mQRCodeView.setDelegate(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mQRCodeView.startCamera();
+//        mQRCodeView.startCamera(Camera.CameraInfo.CAMERA_FACING_FRONT);
+
+        mQRCodeView.showScanRect();
+        mQRCodeView.startSpot();
+    }
+
+    @Override
+    protected void onStop() {
+        mQRCodeView.stopCamera();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mQRCodeView.onDestroy();
+        super.onDestroy();
+    }
+
+    private void vibrate() {
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+        vibrator.vibrate(200);
+    }
+
+    @Override
+    public void onScanQRCodeSuccess(String result) {
+        Log.i(TAG, "result:" + result);
+        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        vibrate();
+        mQRCodeView.startSpot();
+    }
+
+    @Override
+    public void onScanQRCodeOpenCameraError() {
+        Log.e(TAG, "打开相机出错");
+    }
+}
